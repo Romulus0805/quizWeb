11af76faaff31194126dee1a039cf9e9178d088f
Romulus0805 <emb_14@icloud.com>
2024-09-04 13:26:21 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".wrong").click(function(){
-    $(this).css({"background-color": #d63636, "color": white});
+    $(this).css({"background-color": "#d63636", "color": "white"});
   });
   $(".right").click(function(){
-    $(this).css({"background-color": #32a852, "color": white});
+    $(this).css({"background-color": "#32a852", "color": "white"});
   });
 });
