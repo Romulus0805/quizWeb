c6589c972130aae2df1b5fe6aa88ad2570342c07
Romulus0805 <emb_14@icloud.com>
2024-09-04 13:07:36 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $(".wrong").click(function(){
+    $(this).css({"background-color": red, "color": white});
+  });
+  $(".right").click(function(){
     $(this).css({"background-color": green, "color": white});
   });
 });
